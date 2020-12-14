0e346b15d18e5eda4dfbbd96228804109e694470
John Hicks <jphicks@odu.edu>
2020-12-14 09:42:05 -0500

    Baskervillechild technical setup
    - Fixes functions.php
    - Enqueues only our styles/scripts, as parent's are already enqueued
    - Adds our shortcode into the clt-events single and archive, remove
      cruft from archive page. (It seems like the archive page was creating
      an archive box for each post - seems like a mistake)
    - Adds child theme to local Docker setup
    - TODO: lots of styling needed, including fixes on the parent.
    - TODO: accessibility markup/design - theme does not seem to have strong support
    
    Co-author: Ebcosio

--- Baskervillechild/single-clt-events.php
+++ Baskervillechild/single-clt-events.php
@@ -1,46 +1,104 @@
-<?php
-/**
- * The template for displaying a month's worth of CLT events
- */
+<?php get_header(); ?>
 
-get_header(); ?>
-
-<div class="wrap">
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
-
-            <?php
-            // Start the Loop.
-            while ( have_posts() ) :
-                the_post();
-
-                if (class_exists('CLT_EVENTS_CPT')) :
-                    echo do_shortcode( '[ces_events '
-                        . 'start_date="' . CLT_EVENTS_CPT::startDateFromSlug() . '" '
-                        . 'end_date="' . CLT_EVENTS_CPT::endDateFromSlug()
-                        . '" ]' );
-                endif;
-
-                the_content();
-
-                the_post_navigation(
-                    array(
-                        'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'twentyseventeen' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '</span>%title</span>',
-                        'next_text' => '<span class="screen-reader-text">' . __( 'Next Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'twentyseventeen' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ) . '</span></span>',
-                    )
-                );
-
-            endwhile; // End the loop.
-            ?>
-
-        </main><!-- #main -->
-    </div><!-- #primary -->
-   <aside  class="widget-area clt-sidebar" role="complementary" aria-label="CLT News sidebar">
-<?php dynamic_sidebar( 'sidebar-1' ); ?>
-		
-</aside>
+<div class="wrapper section medium-padding" id="site-content">
+										
+	<div class="section-inner">
 	
-</div><!-- .wrap -->
+		<div class="content fleft">
+												        
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+						
+				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-<?php
-get_footer();
+					<?php
+					$format = get_post_format();
+				    ?>
+					
+				
+					<div class="post-header">
+
+						<?php if ( get_the_title() ) : ?>
+						
+						    <h1 class="post-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+
+						<?php endif; ?>
+					    
+					</div><!-- .post-header -->
+														                                    	    
+					<div class="post-content">
+						
+						<?php 
+                        if (class_exists('CLT_EVENTS_CPT')) :
+                            echo do_shortcode( '[ces_events '
+                                . 'start_date="' . CLT_EVENTS_CPT::startDateFromSlug() . '" '
+                                . 'end_date="' . CLT_EVENTS_CPT::endDateFromSlug()
+                                . '" ]' );
+                        endif;
+                        the_content();                                                                                      
+						wp_link_pages();
+						?>
+						
+						<div class="clear"></div>
+									        
+					</div><!-- .post-content -->
+					            					
+					<div class="post-meta-container">
+						
+						<div class="post-meta">
+						
+							<p class="post-date"><?php the_time( get_option( 'date_format' ) ); ?></p>
+							
+							<div class="clear"></div>
+							
+							<div class="post-nav">
+							
+								<?php
+
+								$prev_post = get_previous_post();
+								$next_post = get_next_post();
+
+								if ( $prev_post ) :
+									?>
+								
+									<a class="post-nav-prev" href="<?php the_permalink( $prev_post->ID ); ?>"><?php _e( 'Previous month', 'baskerville' ); ?></a>
+							
+									<?php 
+								endif; 
+
+								if ( $next_post ) :
+									?>
+									
+									<a class="post-nav-next" href="<?php the_permalink( $next_post->ID ); ?>"><?php _e( 'Next month', 'baskerville' ); ?></a>
+							
+									<?php 
+								endif; 
+								
+								edit_post_link( __( 'Edit CLT event month', 'baskerville' ) ); 
+								
+								?>
+									
+								<div class="clear"></div>
+							
+							</div><!-- .post-nav -->
+						
+						</div><!-- .post-meta -->
+						
+						<div class="clear"></div>
+							
+					</div><!-- .post-meta-container -->
+												                        
+				<?php endwhile; endif; ?>
+		
+			</div><!-- .post -->
+		
+		</div><!-- .content -->
+		
+		<?php get_sidebar(); ?>
+		
+		<div class="clear"></div>
+		
+	</div><!-- .section-inner -->
+
+</div><!-- .wrapper -->
+		
+<?php get_footer(); ?>
