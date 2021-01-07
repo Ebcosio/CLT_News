ad7cd2543d78e3d257aae1b8444869321be93803
John Hicks <jphicks@odu.edu>
2021-01-07 10:02:36 -0500

    [FIX] Allow <tr> to contain <textareas> in TNP editor
    The height=100 for each tr conflicted with a dynamic
    height value added to a focused textarea by TNP's JS.
    Because the row as 100px and the textarea 400px, we
    got a visual bug.
    
    This commit also removes additional styles that weren't
    strictly necessary. It seems TNP does a good job of
    of styling the rows without the additions.

--- clt-events/inc/newsletter/blocks/two-column-event/options.php
+++ clt-events/inc/newsletter/blocks/two-column-event/options.php
@@ -14,54 +14,54 @@ $options = array_merge($default_options, $options);
 
 <table class="form-table">
     <tr>
-      <td style="width: 100%; height: 100px; margin: 10px;">
+      <td>
           <p>
               Event Date
                <?php $controls->textarea('date') ?>
           </p>
       </td>
 
-        <td style="width: 100%;height: 100px;  margin: 10px;">
+        <td>
             <p>
                 Event title
                  <?php $controls->textarea('title') ?>
             </p>
         </td>
-        <td style="width: 100%; height: 100px; margin: 10px;">
+        <td>
             <p>
                 Event info
                  <?php $controls->textarea('info') ?>
             </p>
         </td>
 
-         <td style="width: 100%; height: 100px; margin: 10px;">
+         <td>
             <p>
                 Event href (enter as plain text)
                  <?php $controls->textarea('event_href') ?>
             </p>
         </td>
 
-        <td style="width: 100%; height: 100px; margin: 10px;">
+        <td>
             <p>
                 Event 2 Date
                  <?php $controls->textarea('date2') ?>
             </p>
         </td>
 
-          <td style="width: 100%;height: 100px;  margin: 10px;">
+          <td>
               <p>
                   Event 2 title
                    <?php $controls->textarea('title2') ?>
               </p>
           </td>
-          <td style="width: 100%; height: 100px; margin: 10px;">
+          <td>
               <p>
                   Event 2 info
                    <?php $controls->textarea('info2') ?>
               </p>
           </td>
 
-           <td style="width: 100%; height: 100px; margin: 10px;">
+           <td>
               <p>
                   Event 2 href (enter as plain text)
                    <?php $controls->textarea('event_href2') ?>
@@ -69,15 +69,15 @@ $options = array_merge($default_options, $options);
           </td>
 
 
-        <td style="width: 100%; margin: 10px;">
+        <td>
          <p>Font Family (enter property value as font-family(s) and generic-family at the end): </p> <br/>
           <?php $controls->textarea('font_family') ?>
        </td>
-       <td style="width: 100%; margin: 10px;">
+       <td>
          <p>Font size in pixels: </p> <br/>
            <?php $controls->text('font_size') ?>
        </td>
-       <td style="width: 100%; margin: 10px;">
+       <td>
          <p>Font Color (as hex value, or html name): </p> <br/>
            <?php $controls->text('font_color') ?>
        </td>
