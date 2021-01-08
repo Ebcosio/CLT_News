f8586d6376d353473fe84304b91152860c120f4a
Ebcosio <43482687+Ebcosio@users.noreply.github.com>
2021-01-08 11:24:38 -0500

    updated in line styling
    Added <th> for date values in each event

--- clt-events/inc/newsletter/blocks/two-column-event/block.php
+++ clt-events/inc/newsletter/blocks/two-column-event/block.php
@@ -52,12 +52,19 @@ $options = array_merge($default_options, $options);
 
 </style>
 <table width="100%" cellpadding="0" align="center" cellspacing="0">
+    
+    <tr style="margin-bottom: 0;">
+    <th style="font-size: 18px; font-weight: bold;"><?php echo $options['date'] ?></th>
+    <th style="font-size: 18px; font-weight: bold;"><?php echo $options['date2'] ?></th>
+  </tr>
+    
     <tr>
         <td width="50%"  align="center" inline-class="html-td" >
  
-               <div style="border-right: solid #dddddd; border-right-width: 1px;">
-                  <p style="font-size: 18px; font-weight: bold; min-height: 2em;"> <?php echo $options['date'] ?></p>
-            <p style="font-size: 16px; line-height: 1.5; color: #0e5080; font-weight: 700; min-height: 2em;"> <?php echo $options['title'] ?> </p>
+               <div style="border-right: solid #dddddd; border-right-width: 1px; padding: 15px; margin-bottom: 20px;">
+                
+            <p style="font-size: 16px; line-height: 1.5; color: #0e5080;
+            font-weight: 700; min-height: 2em; margin-top: 0;"> <?php echo $options['title'] ?> </p>
             <p style="font-size: 16px; line-height: 1;"> <?php echo $options['info'] ?> </p>
             <p><a href=<?php echo $options['event_href']  ?> target="_blank"><?php echo $options['link_text'] ?></a></p>
 
@@ -66,9 +73,10 @@ $options = array_merge($default_options, $options);
 
         <td width="50%"  align="center" inline-class="html-td">
  
-               <div style="line-height: 1;">
-                  <p style="font-size: 18px; font-weight: bold; min-height: 2em;"> <?php echo $options['date2'] ?></p>
-            <p style="font-size: 16px; line-height: 1.5; color: #0e5080; font-weight: 700; min-height: 2em;"> <?php echo $options['title2'] ?> </p>
+               <div style="line-height: 1; padding: 15px; margin-bottom: 20px;">
+                
+            <p style="font-size: 16px; line-height: 1.5; color: #0e5080;
+            font-weight: 700; min-height: 2em; margin-top: 0;"> <?php echo $options['title2'] ?> </p>
             <p style="font-size: 16px; line-height: 1;"> <?php echo $options['info2'] ?> </p>
             <p><a href=<?php echo $options['event_href2']  ?> target="_blank"><?php echo $options['link_text2'] ?></a></p>
 
@@ -78,6 +86,4 @@ $options = array_merge($default_options, $options);
 
 
 </table>
-
-
 
